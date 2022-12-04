36001d9e52e1b329e2c275b7635fd07d5d2c9561
Mahedi-junior <mahedih.cse@gmail.com>
2022-12-04 15:14:13 +0600

    dragon news completed

--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -16,13 +16,15 @@ export const routes = createBrowserRouter([
       {
         path: "/",
         element: <Home></Home>,
-        loader: () => fetch("http://localhost:5000/news"),
+        loader: () => fetch("https://dragon-news-server-m62.vercel.app/news"),
       },
       {
         path: "/category/:id",
         element: <Category></Category>,
         loader: ({ params }) =>
-          fetch(`http://localhost:5000/category/${params.id}`),
+          fetch(
+            `https://dragon-news-server-m62.vercel.app/category/${params.id}`
+          ),
       },
       {
         path: "/news/:id",
@@ -32,7 +34,7 @@ export const routes = createBrowserRouter([
           </PrivateRoute>
         ),
         loader: ({ params }) =>
-          fetch(`http://localhost:5000/news/${params.id}`),
+          fetch(`https://dragon-news-server-m62.vercel.app/news/${params.id}`),
       },
       {
         path: "/login",
